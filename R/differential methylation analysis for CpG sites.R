b35314e093b5da66d3f3814c211cad6b35c012e3
Minsun Jeon <60573120+minsunjeon@users.noreply.github.com>
2022-07-12 15:41:06 -0500

    Revision.1

--- R/differential methylation analysis for CpG sites.R
+++ R/differential methylation analysis for CpG sites.R
@@ -1,8 +1,9 @@
-#R package for differential methylation analysis
+#Differential methylation analysis for CpG sites
 
-#This package is only compatible with MethylDackel output files, which are .bedgraph files. 
+#NOTE:This package is only compatible with MethylDackel output files, which are .bedgraph files. 
 
-#1. Reading in bedgraphs (methyldackel output file) and joining them together
+#1. Read in .bedgraphs and join them together
+#This function requires "name", which is the sample name chosen under the user's discretion.
 
 read.bedgraph <- function(x,name){
   library("tibble")
@@ -14,6 +15,8 @@ read.bedgraph <- function(x,name){
   return(meth.table)
 }
 
+#Theoretically, each sample has its own .bedgraph file. 
+#Thus, this function joins the methylation results for all the samples into one table.
 join.bedgraph <- function(x,...){
   library(plyr)
   full=list(...)
@@ -21,7 +24,8 @@ join.bedgraph <- function(x,...){
   return(FullDat)
 }
 
-#2.Calculate total reads for each CpG site and add to the full table (the output of this function = input for CpG islands)
+#2.Calculate total reads for each CpG site and add to the full table
+#(the output of this function will be used as the input for the mean shift application)
 total.reads <- function(x){
   tot=c()
   tot=apply(x[,-c(1:3)],1, sum, na.rm = TRUE)
@@ -32,10 +36,13 @@ total.reads <- function(x){
 
 #3.Remove NAs if there are NAs for all samples in one group
 #The user needs to define groups (Ex. group1 = normal, group2 = cancer)
+#df is the table obtained from total.reads()
+#x: methylated columns for group 1
+#y: methylated columns for group 2
 filter.dataset <- function(x,y,df){
   count_func <- function(x) sum(!is.na(x)) 
-  df$group1.count <<- apply(x, 1, count_func) #x: methylated columns for group 1
-  df$group2.count <<- apply(y, 1, count_func) #y: methylated columns for group 2
+  df$group1.count <<- apply(x, 1, count_func) 
+  df$group2.count <<- apply(y, 1, count_func) 
   df$group1.count <<- as.numeric(z$group1.count)
   df$group2.count <<- as.numeric(z$group2.count)
   filtered <- subset(df,group1.count != 0 & group2.count != 0) #subset rows that has at least one set of values (meth + unmeth) for both groups
